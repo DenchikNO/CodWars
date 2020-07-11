bb061c146a23c57dc451b8424db91fa076ac5407
DenchikNO <44582128+DenchikNO@users.noreply.github.com>
2020-07-11 12:55:05 +0300

    подсчет букв абзаца

---
+++ src/main/java/com/exp/codwars/wordcount/EnglishWords.java
@@ -0,0 +1,27 @@
+package com.exp.codwars.wordcount;
+
+import javax.swing.text.ParagraphView;
+import java.security.PrivateKey;
+
+public class EnglishWords {
+
+    private String paragraph = "We have been made aware of three customers who tested positive for Covid-19 and recently traveled on Endeavor from Atlanta to Albany. We are following the guidance of local healthcare officials and the Centers for Disease Control and Prevention. The health and safety of our customers and crews is our top priority.";
+
+    private char[] charParagraph = paragraph
+            .toLowerCase()
+            .toCharArray();
+
+    private void setCharParagraph() {
+        for (char c : charParagraph) {
+            int count = 0;
+            //
+
+            //
+
+        }
+    }
+
+    private void equalsChar(char c){
+        if (c[0]==)
+    }
+}
---
+++ src/main/java/com/exp/codwars/wordcount/WordCount.java
@@ -0,0 +1,9 @@
+package com.exp.codwars.wordcount;
+
+public class WordCount {
+    public static void main(String[] args) {
+
+
+
+    }
+}
