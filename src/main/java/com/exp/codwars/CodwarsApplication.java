8262ba569a2c3d2ddd150ba7cbcdff6ab5e16e1c
DenchikNO <44582128+DenchikNO@users.noreply.github.com>
2020-07-11 11:56:13 +0300

    init

---
+++ src/main/java/com/exp/codwars/CodwarsApplication.java
@@ -0,0 +1,13 @@
+package com.exp.codwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodwarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodwarsApplication.class, args);
+	}
+
+}
